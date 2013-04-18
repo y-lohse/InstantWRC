b097a45ad43e75725730f2759a4bbfa223d98159
Yannick Lohse <glorf.travian@gmail.com>
2013-04-18 22:26:34 +0200

    Stage Model

---
+++ Model/Stage.php
@@ -0,0 +1,18 @@
+<?php
+class Stage extends AppModel{
+	public $useTable = 'stage';
+	public $alias = 'Stage';
+	public $primaryKey = 'stage_id';
+	
+	public function getStages($rally_id){
+		$fields = array('stage_id'=>'id',
+						'stage_name'=>'name',
+						'stage_distance'=>'distance');
+		$conditions = array('fk_rally_id'=>$rally_id);
+		
+		$params= array('fields'=>$this->fieldsAs($fields),
+						'conditions'=>$conditions);
+		return $this->find('all', $params);
+	}
+}
+?>
