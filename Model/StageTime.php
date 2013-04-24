34dc13ac5310c6f065b5e83f1a3ad596e128045a
Yannick Lohse <glorf.travian@gmail.com>
2013-04-24 23:19:59 +0200

    Fix bug MAJ stages

--- Model/StageTime.php
+++ Model/StageTime.php
@@ -3,6 +3,12 @@ class StageTime extends AppModel{
 	public $useTable = 'stage_time';
 	public $alias = 'StageTime';
 	
+	public function isRegistered($driver_id, $stage_id){
+		$conditions = array('fk_driver_id'=>$driver_id,
+							 'fk_stage_id'=>$stage_id);
+		return $this->find('count', array('conditions'=>$conditions));
+	}
+	
 	public function registerTime($driver_id, $stage_id, $time){
 		$this->create();
 		$this->set('fk_driver_id', $driver_id);
