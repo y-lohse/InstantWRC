04770994b956008efe0bf540a209882da16a3be1
Yannick Lohse <glorf.travian@gmail.com>
2013-04-27 00:16:05 +0200

    Création des spéciales avec fuseau horaire

--- Model/Rally.php
+++ Model/Rally.php
@@ -4,6 +4,16 @@ class Rally extends AppModel{
 	public $alias = 'Rally';
 	public $primaryKey = 'rally_id';
 	
+	public function getTimezone($rally_id){
+		$fields = array('Rally.rally_timezone'=>'timezone');
+		$conditions = array('Rally.rally_id'=>$rally_id);
+		
+		$params = array('fields'=>$this->fieldsAs($fields),
+						'conditions'=>$conditions);
+		$result = $this->find('first', $params);
+		return $result['Rally']['timezone'];
+	}
+	
 	public function getRunningRally($when){
 		$fields = array('Rally.rally_id'=>'id',
 						'Rally.rally_url'=>'url');
