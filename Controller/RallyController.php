2083df4c66a0d5628e1c6fe6ae25711449bb01de
Yannick Lohse <glorf.travian@gmail.com>
2013-04-28 11:38:30 +0200

    Calcul des différences de temps #10

--- Controller/RallyController.php
+++ Controller/RallyController.php
@@ -9,32 +9,28 @@ class RallyController extends AppController {
 	
 	public function view($rally_id){
 		$rally_id = (int)$rally_id;
+		App::import('Vendor', 'WrcTime');
 		
 		$rawTimes = $this->Overall->getOverALlTimes($rally_id);
 		$times = array();
-		foreach ($rawTimes as $time){
-			//conversion du chrono en timestamp (en dizieme de secondes)
-			$splitted = preg_split('/[:.]/', $time['Overall']['time']);
-			$timestamp = 0;
-			$splitted = array_reverse($splitted);
-			
-			if (count($splitted) === 4) $timestamp += $splitted[3]*60*60*10;
-			if (count($splitted) >= 3) $timestamp += $splitted[2]*60*10;
-			$timestamp += $splitted[1]*10 + $splitted[0];
-			
+		foreach ($rawTimes as $time){	
 			array_push($times, array('driver'=>$time['Driver']['name'],
-									 'time'=>$time['Overall']['time'],
-									 'timestamp'=>$timestamp,
+									 'timestamp'=>WrcTime::toTimestamp($time['Overall']['time']),
 									 'retired'=>(bool)$time['Overall']['retired'],
 									 'last_stage'=>$time[0]['last_stage_id']));
 		}
 		
 		usort($times, array($this, 'sortTimes'));
 		
+		$best = $previous = $times[0]['timestamp'];
+		
 		foreach ($times as $index=>&$time){
 			if ($time['retired']) continue;
 			
 			$time['rank'] = $index+1;
+			$time['best'] = WrcTime::toTimestring($time['timestamp'] - $best);
+			$time['previous'] = WrcTime::toTimestring($time['timestamp'] - $previous);
+			$previous = $time['timestamp'];
 		}
 	
 		$this->set('times', $times);
