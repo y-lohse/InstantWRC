490960d30ea0c81af2de62337327dc27bc0bbf8c
Yannick Lohse <glorf.travian@gmail.com>
2013-05-04 16:39:50 +0200

    Début du pull to refresh #27

--- webroot/js/directives.js
+++ webroot/js/directives.js
@@ -1,8 +1,43 @@
 angular.module('directives', [])
-.directive('pull', function(){
+.directive('pullToRefresh', function($document, $window){
 	return function(scope, iElement, iAttrs){
-		iElement.bind('click', function(){
-			alert('ok');
+		iElement.css({'height': 0, 'overflow': 'hidden'});
+		var initOffset = 15,
+			refreshOffset = 100,
+			refreshOnRelease = false,
+			touchStart = 0,
+			prevOffset = 0;
+		
+		$document.bind('touchstart', function(e){
+			touchStart = e.touches[0].pageY;
 		});
+		$document.bind('touchend', function(e){
+			if (refreshOnRelease) alert('reload');
+			shutDown();
+		});
+		
+		$document.bind('touchmove', function(e){
+			var dif = e.touches[0].pageY - touchStart;
+			var scrollTop = $window.pageYOffset;
+			var offset = dif - initOffset;
+			
+			if (scrollTop == 0 && dif > 0) e.preventDefault();
+			if (scrollTop == 0 && dif > initOffset && prevOffset != offset) refresh(offset);
+		});
+		
+		var refresh = function(offset){
+			prevOffset = offset;
+			refreshOnRelease = (offset >= refreshOffset) ? true : false;
+			
+			offset = Math.min(offset, refreshOffset);
+			iElement.css({'height': offset+'px'});
+			iElement.html(offset);
+		}
+		
+		var shutDown = function(){
+			iElement.css({height: 0});
+			touchStart = prevOffset = 0;
+			refreshOnRelease = false;
+		}
 	}
 });
