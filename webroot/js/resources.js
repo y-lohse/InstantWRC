fab0bd1a9dad35d7d8398fc06ba1277f3440b54a
Yannick Lohse <glorf.travian@gmail.com>
2013-05-04 21:03:39 +0200

    Correction de bug

--- webroot/js/resources.js
+++ webroot/js/resources.js
@@ -2,7 +2,8 @@ angular.module('WrcService', ['ngResource']).
 factory('Rally', function($resource){
 	return $resource(
 		'/rally/:rallyId.json',
-		{stages: {method: 'GET', url: '/rally/stages/:rallyId.json'}}
+		{},
+		{'stages': {method: 'GET', url: '/rally/stages/:rallyId.json'}}
 	);
 }).
 factory('Stage', function($resource){
