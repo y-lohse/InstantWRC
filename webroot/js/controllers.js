68a58ca18db23813b36dab87d92d0834ad3b5f51
Yannick Lohse <glorf.travian@gmail.com>
2013-05-04 20:49:13 +0200

    Suppression de reliquat

--- webroot/js/controllers.js
+++ webroot/js/controllers.js
@@ -1,34 +1,47 @@
 InstantWRC.controller('RankingController', function(){
 });
-
+counter = 0;
 InstantWRC.controller('RallyController', function($scope, $http, Rally){
 	$scope.showStage = false;
 	$scope.firstStage = '';
 	
-	Rally.get({rallyId: $scope.rally_id}, function(data){
-		$scope.times = data.times;
-		$scope.stagename = data.stagename;
-		
-		//plusieurs spéciales concurentes?
-		$scope.firstStage = $scope.times[0].last_stage;
-		for (var i = 1, l = $scope.times.length; i < l; i++){
-			if (!$scope.times[i].retired && $scope.times[i].last_stage != $scope.firstStage){
-				$scope.showStage = true;
-				break;
+	$scope.fetchData = function(){
+		counter++;
+		Rally.get({rallyId: $scope.rally_id}, function(data){
+			$scope.times = data.times.slice(0, counter);
+			$scope.stagename = data.stagename;
+			
+			//plusieurs spéciales concurentes?
+			$scope.firstStage = $scope.times[0].last_stage;
+			for (var i = 1, l = $scope.times.length; i < l; i++){
+				if (!$scope.times[i].retired && $scope.times[i].last_stage != $scope.firstStage){
+					$scope.showStage = true;
+					break;
+				}
 			}
-		}
-	});
+		});
+	};
+	
+	$scope.fetchData();
 });
 
 InstantWRC.controller('StagesController', function($scope, $http, Rally){
-	Rally.stages({rallyId: $scope.rally_id}, function(data){
-		$scope.stages = data.stages;
-	});
+	$scope.fetchData = function(){
+		Rally.stages({rallyId: $scope.rally_id}, function(data){
+			$scope.stages = data.stages;
+		});
+	};
+	
+	$scope.fetchData();
 });
 
 InstantWRC.controller('StageController', function($scope, $http, $routeParams, Stage){
-	Stage.get({stageId: $routeParams.stageId}, function(data){
-		$scope.times = data.times;
-		$scope.stagename = data.stagename;
-	});
+	$scope.fetchData = function(){
+		Stage.get({stageId: $routeParams.stageId}, function(data){
+			$scope.times = data.times;
+			$scope.stagename = data.stagename;
+		});
+	};
+	
+	$scope.fetchData();
 });
